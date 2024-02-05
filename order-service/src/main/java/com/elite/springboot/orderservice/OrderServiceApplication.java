410c0d6df826d2a748c119c97d5e6d0446a32f79
ashvani1 <ashvani.vashist@gmail.com>
2024-02-05 18:52:35 +0530

    FeignClient implementation to call ProductService from OrderService

--- order-service/src/main/java/com/elite/springboot/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/elite/springboot/orderservice/OrderServiceApplication.java
@@ -3,9 +3,11 @@ package com.elite.springboot.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.elite.springboot.orderservice.entity"})
+@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/elite/springboot/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/elite/springboot/orderservice/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.elite.springboot.orderservice.service;
 
 import com.elite.springboot.orderservice.entity.Order;
+import com.elite.springboot.orderservice.external.client.ProductService;
 import com.elite.springboot.orderservice.model.OrderRequest;
 import com.elite.springboot.orderservice.repository.IOrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -14,6 +15,9 @@ import java.time.Instant;
 public class OrderService implements IOrderService{
 
     @Autowired
+    private ProductService productService;
+
+    @Autowired
     IOrderRepository orderRepository;
     @Override
     public long placeOrder(OrderRequest orderRequest) {
@@ -23,6 +27,12 @@ public class OrderService implements IOrderService{
         //call payment service to do payment. once payment is successful we can mark order as successful. else mark as pending/cancel
 
         //to save order we have to convert orderRequest into order entity.
+
+        log.info("placing order request with input :: {}", orderRequest);
+        //call product service
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
+        log.info("creating order with status created");
         Order order = Order.builder()
                 .amount(orderRequest.getTotalAmount())
                 .quantity(orderRequest.getQuantity())
