c73de364e793ce1d964760c4103a832136110075
ashvani1 <ashvani.vashist@gmail.com>
2024-02-24 08:05:18 +0530

    working: modification to generate docker images for all services

--- payment-service/src/main/java/com/elite/springboot/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/elite/springboot/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.elite.springboot.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.elite.springboot.paymentservice.entity"})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/elite/springboot/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/elite/springboot/productservice/ProductServiceApplication.java
@@ -2,10 +2,12 @@ package com.elite.springboot.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EntityScan(basePackages = {"com.elite.springboot.productservice.entity"})
 public class ProductServiceApplication  {
 
 	public static void main(String[] args) {
